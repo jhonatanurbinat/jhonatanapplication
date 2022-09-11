9db235f4391dad985d2d2c10b8822e34766dde30
Jhonatan Urbina <jhonatanurbinat@gmail.com>
2022-09-11 15:54:45 -0500

    commit

--- __tests__/test.t.js
+++ __tests__/test.t.js
@@ -2,23 +2,41 @@
 
 // import { index } from '../routes/rutas'
 
-const { index } = require('../routes/rutas');
+// const { index } = require('../routes/rutas');
 
+const { login } = require('../controllers/auth');
+
+// const request = require("supertest");
 
 //import { login } from '../routes/auth.js';
 
 describe('Test Handlers', function () {
 
-    test('responds to /', () => {
-        const req = {  };
+    test('Validating GET Response Method equals ERROR /', () => {
+        const req = { method: 'GET' };
 
         const res = { text: '',
             send: function(input) { this.text = input } 
         };
-        index(req, res);
-        //expect(true).toEqual(true);
-        expect(res.text).toEqual('hello world!');
+        login(req, res);
+        // console.log(req);
+        // console.log(res);
+        
+        expect(res.text).toEqual('ERROR');
     });
 
+    test('Validating GET Response Method equals DELETE /', () => {
+        const req = { method: 'DELETE' };
+
+        const res = { text: '',
+            send: function(input) { this.text = input } 
+        };
+        login(req, res);
+        // console.log(req);
+        // console.log(res);
+        
+        expect(res.text).toEqual('ERROR');
+    });
+    
 
 });
